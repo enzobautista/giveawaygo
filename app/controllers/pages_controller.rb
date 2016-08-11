b4ab661e5a08a20e5bc1a095ad363aceb4159957
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2016-08-11 13:32:19 +0800

    fixed database

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,55 +29,55 @@ class PagesController < ApplicationController
         if !user_game_question.present?
           if (@game.id == 1) #Website Go, pwede rin if game.type == "Enzo Go" kaso mas efficient if u compare integers
             if (@question.number == 1)
-              @question_text = "Acc to bongbong marcos, magkano ang pamasahe ngayon sa jeep?"
-              @choices = [["6 pesos",1],["4 pesos",2],["10 pesos",3]]
+              @question_text = "What is our theme?"
+              @choices = [["Creating Possibilities",1],["Evolving ICT: Enabling, Transforming, Redefining the Future",2],["ICT: Bolder, Broader, Easier, Faster, Stronger, Wiser",3]]
             elsif (@question.number == 2)
-              @question_text = "What is El Nino?"
-              @choices = [["Drought. We need to start planting trees and we will achieve la nina",1],["Sto Nino ba yan pit senyor!",2],["Alam ko lang nino si nino muhlach",3]]
+              @question_text = "How long has the ICT summit been running?"
+              @choices = [["10 years",1],["5 years",2],["15 years",3]]
             elsif (@question.number == 3)
-              @question_text = "Who dat who dat?"
-              @choices = [["La pong tao rito",1],["This is real this is me",2],["I-G-G-Y!",3]]
+              @question_text = "How many Ayala Group companies are participating in the "
+              @choices = [["7",1],["10",2],["13",3]]
             elsif (@question.number == 4)
-              @question_text = "Acc to bongbong marcos, magkano ang pamasahe ngayon sa jeep?"
-              @choices = [["6 pesos",1],["4 pesos",2],["10 pesos",3]]
+              @question_text = "How many CIO forum members are there?"
+              @choices = [["7",1],["10",2],["16",3]]
             elsif (@question.number == 5)
-              @question_text = "Acc to bongbong marcos, magkano ang pamasahe ngayon sa jeep?"
-              @choices = [["6 pesos",1],["4 pesos",2],["10 pesos",3]]
+              @question_text = "How many subject tracks are there for the breakout sessions?"
+              @choices = [["3",1],["6",2],["9",3]]
             else
             end
           end
           if (@game.id == 2) #Sponsors Go
             if (@question.number == 1)
-              @question_text = "Do u feel any pressure right now?"
-              @choices = [["Syempre wala! Confident!",1],["Dasal lang",2],["No i dont feel any pressure right now",3]]
+              @question_text = "Which of the following companies is NOT a Platinum Sponsor"
+              @choices = [["Accenture",1],["Globe Business",2],["Barhead",5],["Fis",4],["Oracle",5],["Altaire i Solutions, Inc.",6],["Huawei",7],["Brother",8],["Ramco",9]]
             elsif (@question.number == 2)
-              @question_text = "Do u support the RH Law?"
-              @choices = [["Yes, with reservations",1],["Yes",2],["No",3]]
+              @question_text = "Which of the following companies is NOT a Session Sponsor?"
+              @choices = [["Teradata",1],["Forecasting and Planning Technologies, Inc.",2],["Tech Mahindra",3],["SAP",4],["PWC",5],["Infomax",6],["Servicenow Nexus",7]]
             elsif (@question.number == 3)
-              @question_text = "What are ur reservations about the RH Law?"
-              @choices = [["Ano ba ang reservations",1],["Kailangan pa ba sagutin yan?",2],["Pills!",3]]
+              @question_text = "Which of the following companies is a Gold Sponsor?"
+              @choices = [["Fujitsu",1],["Global Sapphire Inc.",2],["Questronix",3]]
             elsif (@question.number == 4)
-              @question_text = "Do u feel any pressure right now?"
-              @choices = [["Syempre wala! Confident!",1],["Dasal lang",2],["No i dont feel any pressure right now",3]]
+              @question_text = "Which of the following companies is NOT a Silver Sponsor?"
+              @choices = [["Fortinet",1],["SAS",2],["Fis",3],["Network Manager",4],["Tech Mahindra",5],["Intel Security",6],["Anaplan",7],["Fujitsu",8],["Software One",9],["Trends & Technologies",10],["Brother",11]]
             elsif (@question.number == 5)
-              @question_text = "Do u feel any pressure right now?"
-              @choices = [["Syempre wala! Confident!",1],["Dasal lang",2],["No i dont feel any pressure right now",3]]
+              @question_text = "Which of the following companies is NOT an Exhibit Booth Sponsor?"
+              @choices = [["Accenture",1],["SMS",2],["EVI Distribution, Inc.",3],["Multicore Technology Resources, Inc.",4],["Questronix",5],["Equicom, Inc.",6],["Cloudtech",7],["Fasttrack",8],["Net4Site",9],["Inaveit",10]]
             else
             end
           end
           if (@game.id == 3) #Summit Go
             if (@question.number == 1)
-              @question_text = "Do u feel any pressure right now?"
-              @choices = [["Syempre wala! Confident!",1],["Dasal lang",2],["No i dont feel any pressure right now",3]]
+              @question_text = "Who will be giving the Welcome Remarks for the Summit?"
+              @choices = [["Fernando Zobel de Ayala",1],["Jaime Zobel de Ayala",2],["Jaime Augusto Zobel de Ayala",3]
             elsif (@question.number == 2)
-              @question_text = "Do u support the RH Law?"
-              @choices = [["Yes, with reservations",1],["Yes",2],["No",3]]
+              @question_text = "Atty. Rodolfo Salalima is the Secretary of which newly formed Department?"
+              @choices = [["Department of Information Technology",1],["Department of Information and Communications Technology",2],["Department of Transportation and Communications",3]]
             elsif (@question.number == 3)
-              @question_text = "What are ur reservations about the RH Law?"
-              @choices = [["Ano ba ang reservations",1],["Kailangan pa ba sagutin yan?",2],["Pills!",3]]
+              @question_text = "How many breakout sessions will be conducted during the Summit?"
+              @choices = [["65",1],["55",2],["45",3]]
             elsif (@question.number == 4)
-              @question_text = "Do u feel any pressure right now?"
-              @choices = [["Syempre wala! Confident!",1],["Dasal lang",2],["No i dont feel any pressure right now",3]]
+              @question_text = "How many exhibit booths are there in the Summit?"
+              @choices = [["8",1],["18",2],["28",3]]
             else
             end
           end
